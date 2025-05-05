6ee6f9cec63ecf64795c7a1e38b3930b2cf969f3
victorramosj <48687982+victorramosj@users.noreply.github.com>
2025-05-05 11:55:56 -0300

    Update urls.py

--- sigref/sigref/urls.py
+++ sigref/sigref/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from sigref import views # Importe as views do app principal
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('reservas/', include('reservas.urls')),  # Inclui as URLs do app reservas
@@ -20,3 +21,6 @@ urlpatterns = [
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='sigref/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='sigref/password_reset_complete.html'), name='password_reset_complete'),
 ]
+# Servir arquivos de mídia em DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
