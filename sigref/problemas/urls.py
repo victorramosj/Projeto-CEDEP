3c52110b1e47e2b692ec6e0d6b99686c14b27042
LucasdevBastos <lucasviniciusdesousabastos@gmail.com>
2025-05-27 16:47:33 -0300

    update teste

--- sigref/problemas/urls.py
+++ sigref/problemas/urls.py
@@ -6,3 +6,12 @@ router.register(r'lacunas', LacunaViewSet, basename='lacunas')
 router.register(r'problemas-usuario', ProblemaUsuarioViewSet, basename='problemas-usuario')
 
 urlpatterns = router.urls
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('escola/dashboard/', views.problema_dashboard_view, name='problema_dashboard'),
+]
+
