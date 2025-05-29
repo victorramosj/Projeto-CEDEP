c16d01e0fa3fb25267846fd2150e9a4eb02d95fc
victorramosj <48687982+victorramosj@users.noreply.github.com>
2025-05-29 13:44:31 -0300

    update

--- sigref/monitoramento/urls.py
+++ sigref/monitoramento/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     path('escola/<int:escola_id>/questionario/<int:questionario_id>/responder/', views.ResponderQuestionarioView.as_view(), name='responder_questionario'),
 
     # Dashboard da escola
-   
+    
     path('escola/relatar-problema/', views.RelatoProblemaCreateView.as_view(), name='relatar_problema'),
 
     # -----------------------------------------
