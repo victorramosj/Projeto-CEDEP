bfb327e48e913ac09c9ab8702962dc4f1082b720
Victorkaue333 <kaue.alves.pg@gmail.com>
2025-05-30 16:49:28 -0300

    update
    tá funcionando

--- sigref/problemas/admin.py
+++ sigref/problemas/admin.py
@@ -27,3 +27,19 @@ class ProblemaUsuarioAdmin(admin.ModelAdmin):
     def setor_hierarquia(self, obj):
         return obj.setor.hierarquia_completa if obj.setor else 'Geral'
     setor_hierarquia.short_description = 'Setor'
+
+
+from .models import AvisoImportante
+
+@admin.register(AvisoImportante)
+class AvisoImportanteAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'escola', 'prioridade', 'ativo', 'data_expiracao')
+    list_filter = ('escola', 'prioridade', 'ativo')
+
+    # remove o campo do formulário
+    exclude = ('criado_por',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.criado_por_id:
+            obj.criado_por = request.user.greuser
+        super().save_model(request, obj, form, change)
