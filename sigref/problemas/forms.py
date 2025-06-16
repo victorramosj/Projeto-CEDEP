2df5aa0ed885e6cb653c059a810d2629b6aa04fe
LucasdevBastos <lucasviniciusdesousabastos@gmail.com>
2025-06-16 13:25:56 -0300

    Revert "UP"
    This reverts commit da1e58fcc4d6c030f7b781a3b19cd80d0f855f1b.

--- sigref/problemas/forms.py
+++ sigref/problemas/forms.py
@@ -91,51 +91,40 @@ class LacunaForm(forms.ModelForm):
             'carga_horaria': 'Carga Horária',
         }
 
-# Em seu arquivo forms.py
+# forms.py
 from django import forms
-from django.utils import timezone # <-- Importe o timezone
+from .models import AvisoImportante
+
+from django import forms
+from .models import AvisoImportante
+
+from django import forms
 from .models import AvisoImportante
 
 class AvisoForm(forms.ModelForm):
     class Meta:
         model = AvisoImportante
-        
-        # --- CORREÇÃO 1: CAMPOS ---
-        # Listamos APENAS os campos que o usuário realmente edita no modal.
-        # Isso impede que campos como 'ativo' e 'escola' sejam apagados acidentalmente.
-        fields = [
-            'titulo',
-            'mensagem',
-            'prioridade',
-            'data_expiracao',
-        ]
-
+        fields = ['titulo', 'mensagem', 'prioridade', 'data_expiracao', 'escola', 'setor_destino', 'ativo']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'mensagem': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'prioridade': forms.Select(attrs={'class': 'form-select'}),
-            'data_expiracao': forms.DateTimeInput(
-                attrs={'type': 'datetime-local', 'class': 'form-control'},
-                format='%Y-%m-%dT%H:%M'
-            ),
+            'data_expiracao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'escola': forms.Select(attrs={'class': 'form-select'}),
+            'setor_destino': forms.Select(attrs={'class': 'form-select'}),
+            'ativo': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
 
-    # --- CORREÇÃO 2: FUSO HORÁRIO ---
-    # Adicionamos este método para tratar a data de expiração.
-    def clean_data_expiracao(self):
-        # Pega a data já validada pelo Django
-        data = self.cleaned_data.get('data_expiracao')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['titulo'].widget.attrs.update({'class': 'form-control'})
+        self.fields['mensagem'].widget.attrs.update({'class': 'form-control'})
+        self.fields['prioridade'].widget.attrs.update({'class': 'form-select'})
+        self.fields['setor_destino'].widget.attrs.update({'class': 'form-select'})
         
-        # Se a data existir e for "ingênua" (sem fuso horário)...
-        if data and timezone.is_naive(data):
-            # ...a tornamos "consciente", usando o fuso horário padrão do seu projeto (definido em settings.py)
-            return timezone.make_aware(data)
-            
-        # Retorna a data (seja ela já consciente ou nula)
-        return data
-
     def clean_titulo(self):
         titulo = self.cleaned_data.get('titulo')
         if len(titulo) < 5:
             raise forms.ValidationError("O título precisa ter pelo menos 5 caracteres.")
         return titulo
+
